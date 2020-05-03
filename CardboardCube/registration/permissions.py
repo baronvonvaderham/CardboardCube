fe520603accca10f9ab4ac9fb5b9a1b46babdb60
Scott Johnson <baronvonvaderham@gmail.com>
2020-05-03 17:42:03 -0400

    Fix Coverage config

--- CardboardCube/registration/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwner(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to access it
-    """
-    def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
