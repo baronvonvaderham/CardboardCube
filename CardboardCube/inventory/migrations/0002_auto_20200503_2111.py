99ebf059b0c6afb68a5394c8d15cdb871009d158
Scott Johnson <baronvonvaderham@gmail.com>
2020-05-03 17:20:08 -0400

    Remove the dependency manually?

--- CardboardCube/inventory/migrations/0002_auto_20200503_2111.py
+++ CardboardCube/inventory/migrations/0002_auto_20200503_2111.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('card_catalog', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('inventory', '0001_initial'),
     ]
